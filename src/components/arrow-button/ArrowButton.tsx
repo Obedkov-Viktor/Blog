39b12b1c8a8d6e725318e2ae385f64561b0da362
Viktor Obedkov <obedkov.2018@yandex.ru>
2024-06-12 17:15:45 +0300

    Added several files .gitingore

---
+++ src/components/arrow-button/ArrowButton.tsx
@@ -0,0 +1,36 @@
+import arrow from 'src/images/arrow.svg';
+
+import styles from './ArrowButton.module.scss';
+import React from 'react';
+
+/** Функция для обработки открытия/закрытия формы */
+export type OnClick = () => void;
+
+interface ArrowButtonProps {
+	onClick: OnClick;
+	isOpen: boolean;
+}
+
+export const ArrowButton: React.FC<ArrowButtonProps> = ({
+	onClick,
+	isOpen,
+}) => {
+	return (
+		<div
+			role='button'
+			aria-label={
+				isOpen
+					? 'Закрыть форму параметров статьи'
+					: 'Открыть форму параметров статьи'
+			}
+			tabIndex={0}
+			className={`${styles.container} ${isOpen ? styles.container_open : ''}`}
+			onClick={onClick}>
+			<img
+				src={arrow}
+				alt='иконка стрелочки'
+				className={isOpen ? styles.arrow_open : styles.arrow}
+			/>
+		</div>
+	);
+};
---
+++ src/index.tsx
@@ -0,0 +1,43 @@
+import { createRoot } from 'react-dom/client';
+import { StrictMode, CSSProperties, useState } from 'react';
+import clsx from 'clsx';
+
+import { Article } from 'components/article';
+import { ArticleParamsForm } from 'components/article-params-form';
+import { defaultArticleState } from './constants/articleProps';
+
+import './styles/index.scss';
+import styles from './styles/index.module.scss';
+
+const domNode = document.getElementById('root') as HTMLDivElement;
+const root = createRoot(domNode);
+
+const App = () => {
+	const [articleState, setArticleState] = useState(defaultArticleState); // Используем useState для хранения состояния
+
+	return (
+		<div
+			className={clsx(styles.main)}
+			style={
+				{
+					'--font-family': articleState.fontFamilyOption.value,
+					'--font-size': articleState.fontSizeOption.value,
+					'--font-color': articleState.fontColor.value,
+					'--container-width': articleState.contentWidth.value,
+					'--bg-color': articleState.backgroundColor.value,
+				} as CSSProperties
+			}>
+			<ArticleParamsForm
+				formData={articleState}
+				setFormData={setArticleState}
+			/>
+			<Article />
+		</div>
+	);
+};
+
+root.render(
+	<StrictMode>
+		<App />
+	</StrictMode>
+);
